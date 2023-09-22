e2114bb55ec47d4006fa30a28eb2bbe014c1c122
Sean Worrell <spworrell@gmail.com>
2023-09-22 10:18:53 -0400

    DB Built

--- models/Product.js
+++ models/Product.js
@@ -2,6 +2,7 @@
 const { Model, DataTypes } = require('sequelize');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
+const { NUMBER } = require('sequelize');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
@@ -9,7 +10,24 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    
+      id:{
+        type:DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      product_name:{
+        type:DataTypes.STRING,
+        allowNull: false
+      },
+      stock:{
+        type:DataTypes.INTEGER,
+        allowNull: false,
+        default: 10,
+        validator: NUMBER,
+      }
+    
   },
   {
     sequelize,
